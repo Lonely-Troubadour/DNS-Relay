6f7b0e60bc0584cabeae7e2c83ef98a16a19bfb9
Mr.Who <3062618707@qq.com>
2020-09-14 14:51:59 +0800

    Fix getline bug.

--- dbutils.c
+++ dbutils.c
@@ -4,6 +4,65 @@
 #include <unistd.h>
 #include "dbutils.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+/* The original code is public domain -- Will Hartung 4/9/09 */
+/* Modifications, public domain as well, by Antti Haapala, 11/10/17
+   - Switched to getc on 5/23/19 */
+/** 
+Srouce:
+
+https://stackoverflow.com/questions/735126/are-there-alternate-implementations-of-gnu-getline-interface/47229318#47229318
+*/
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    size_t pos;
+    int c;
+
+    if (lineptr == NULL || stream == NULL || n == NULL) {
+        return -1;
+    }
+
+    c = getc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+
+    if (*lineptr == NULL) {
+        *lineptr = malloc(128);
+        if (*lineptr == NULL) {
+            return -1;
+        }
+        *n = 128;
+    }
+
+    pos = 0;
+    while(c != EOF) {
+        if (pos + 1 >= *n) {
+            size_t new_size = *n + (*n >> 2);
+            if (new_size < 128) {
+                new_size = 128;
+            }
+            char *new_ptr = realloc(*lineptr, new_size);
+            if (new_ptr == NULL) {
+                return -1;
+            }
+            *n = new_size;
+            *lineptr = new_ptr;
+        }
+
+        ((unsigned char *)(*lineptr))[pos ++] = c;
+        if (c == '\n') {
+            break;
+        }
+        c = getc(stream);
+    }
+
+    (*lineptr)[pos] = '\0';
+    return pos;
+}
+#endif
+
+
 /**
  * Look up name from the local dns database.
  * ________________________________________
@@ -26,7 +85,6 @@ int lookup (char *target_name, char **db, char *target_addr) {
     if (*db == NULL) {
         *db = (char *) malloc(MAX_LENGTH);
         strcpy(*db, "./data/dnsrelay.txt");
-        printf("--%s--\n", *db);
     }
 
     /* Open local database */
@@ -62,5 +120,5 @@ int lookup (char *target_name, char **db, char *target_addr) {
 }
 
 void print_db(char *db) {
-    
+
 }
